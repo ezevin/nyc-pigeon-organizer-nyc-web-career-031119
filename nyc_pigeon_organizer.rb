12aa0b408184d89a226188b7cacce4460236db95
Elizabeth Zevin <ehzevin@gmail.com>
2019-03-14 00:34:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,20 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   hash = {}
-
   data.each do |key, value|
-             
   value.map do |values, name|
-               
   name.each do |names|
-
+    binding.pry
     hash[names] ||= {}
     hash[names][key]||= []
     hash[names][key] << values.to_s
-          end
+  end
   end
   end 
   hash
 end
-
-#almost there. just needs to pull rest of names into names hash
